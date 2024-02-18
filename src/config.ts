40d995ba39ac2594fa8cceb037ae9d0ee7823d19
YellowD54321 <yellowd54321@gmail.com>
2024-02-18 22:19:41 +0800

    add eviroment condition

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,18 @@
-export const BASE_URL = 'http://localhost:3000';
+let baseUrl = 'http://localhost:3000';
+let apiUrl = 'http://localhost:8888';
+let webSocketUrl = 'ws://localhost:8888';
 
-export const API_URL = 'http://localhost:8888';
-
+if (process.env.NODE_ENV === 'production') {
+  baseUrl = 'https://master.d3vlltn13xlacs.amplifyapp.com/';
+  apiUrl =
+    'http://ec2-54-206-114-250.ap-southeast-2.compute.amazonaws.com:8888';
+  webSocketUrl =
+    'ws://ec2-54-206-114-250.ap-southeast-2.compute.amazonaws.com:8888';
+}
 export const WEBSITE_NAME = 'Pomodoro';
+
+export const BASE_URL = baseUrl;
+
+export const API_URL = apiUrl;
+
+export const WEB_SOCKET_URL = webSocketUrl;
