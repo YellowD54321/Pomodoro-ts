784b6a911f17ecbbaed6c9037877d68442693eac
YellowD54321 <yellowd54321@gmail.com>
2024-02-21 01:36:48 +0800

    add custom dns and remove server port

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,9 @@ let apiUrl = 'http://localhost:8888';
 let webSocketUrl = 'ws://localhost:8888';
 
 if (process.env.NODE_ENV === 'production') {
-  baseUrl = 'https://master.d3vlltn13xlacs.amplifyapp.com';
-  apiUrl =
-    'https://ec2-54-206-114-250.ap-southeast-2.compute.amazonaws.com:8888';
-  webSocketUrl =
-    'wss://ec2-54-206-114-250.ap-southeast-2.compute.amazonaws.com:8888';
+  baseUrl = 'https://pomodiver.com';
+  apiUrl = 'api.pomodiver.com';
+  webSocketUrl = 'wss://api.pomodiver.com';
 }
 export const WEBSITE_NAME = 'Pomodoro';
 
